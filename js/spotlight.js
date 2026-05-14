1b3d8f7e1ffa2dad42faaa2d19eb94dc4a897358
victoria <liuvictoria12@gmail.com>
2026-05-14 13:12:40 -0700

    new updates

---
+++ js/spotlight.js
@@ -0,0 +1,4 @@
+document.addEventListener('mousemove', function (e) {
+  document.body.style.setProperty('--mouse-x', e.clientX + 'px');
+  document.body.style.setProperty('--mouse-y', e.clientY + 'px');
+});
